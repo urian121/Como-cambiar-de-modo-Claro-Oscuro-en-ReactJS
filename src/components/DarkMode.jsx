a4d11c1c2668b5cf51dd3e71fb72f1518fea16bb
URIAN VIERA <urian1213viera@gmail.com>
2024-06-12 20:36:29 -0500

    agregando comentarios

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -1,16 +1,23 @@
 import { useState, useEffect } from "react";
 
 export default function DarkMode() {
+  // Estado para controlar el modo oscuro
   const [darkMode, setDarkMode] = useState(false);
 
+  // useEffect se utiliza para aplicar efectos secundarios cuando cambia darkMode
   useEffect(() => {
-    const body = document.body;
-    const toggle = document.querySelector(".toggle-inner");
+    const body = document.body; // Referencia al elemento <body> del documento
+    const toggle = document.querySelector(".toggle-inner"); // Referencia al elemento con clase .toggle-inner
 
+    // Aplica la clase 'dark-mode' al elemento <body> si darkMode es true, y la quita si es false
     body.classList.toggle("dark-mode", darkMode);
+    // Aplica la clase 'toggle-active' al elemento con clase '.toggle-inner' si darkMode es true, y la quita si es false
     toggle.classList.toggle("toggle-active", darkMode);
-  }, [darkMode]);
 
+    // useEffect se utiliza para aplicar efectos secundarios cuando cambia darkMode
+  }, [darkMode]); // Se ejecuta cada vez que darkMode cambia
+
+  // Función para alternar el estado de darkMode
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
   };
